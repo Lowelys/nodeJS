99db5142079aeb0a41b3014f1cb4b03489006d96
Sergey Belov <48603698+Lowelys@users.noreply.github.com>
2020-03-31 22:01:37 +0300

    update

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,20 @@
+let http = require('http');
 
+let server = http.createServer((req, res) => {
+    console.log('some request');
+    let message = 'yoyo';
+    switch (req.url) {
+        case '/':
+            res.write(`HOME`);
+            break;
+        case '/tasks':
+            res.write(`tasks`);
+            break;
+        default:
+            res.write(`PAGE NOT FOUND`);
+    }
+    res.end();
+});
+
+server.listen(7542);
+console.log(http);
