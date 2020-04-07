3fcb938b68a32cdbe26828151284b8ed81770666
Sergey Belov <48603698+Lowelys@users.noreply.github.com>
2020-04-07 23:20:16 +0300

    nodeJS-012

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import * as axios from "axios";
 
-let arr = [ [], function () {
+let arr = [[], function () {
 
 }];
 
@@ -10,14 +10,29 @@ let [users, setUsers] = arr;
 
 function App() {
     const [users, setUsers] = useState([]);
-
-    useEffect(() => {
+    const gerUsers = () => {
         axios.get("http://localhost:7542/users").then(res => {
             setUsers(res.data);
         });
+    };
+
+
+    useEffect(() => {
+gerUsers();
     }, []);
-    return (
-        <div>{users.map(u => <div>{u.name}</div>)}</div>
+
+    const creatUser = () => {
+        axios.post("http://localhost:7542/users")
+            .then(res => {
+               gerUsers();
+                });
+    };
+    return (<>
+            <div>
+                <button onClick={creatUser}>Creat new</button>
+            </div>
+            <div>{users.map(u => <div>{u.name}</div>)}</div>
+        </>
     );
 }
 
